771068450f533e33c64a8e7c053a3f89f4996f82
kingkunal26 <kunalchoithani2013@gmail.com>
2023-12-13 14:16:52 +0530

    first commit

---
+++ src/com/fliplkart/demo/DemoApp.java
@@ -0,0 +1,11 @@
+package com.fliplkart.demo;
+
+public class DemoApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is my first App");
+
+	}
+
+}
